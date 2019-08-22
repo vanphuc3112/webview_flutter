9c99afa53eb339e35e1838d0fe7975ef00ebf984
rin.lv <rinlv@sigma-solutions.eu>
2019-08-22 17:52:10 +0700

    update code setCookies for ios

--- ios/Classes/FLTCookieManager.m
+++ ios/Classes/FLTCookieManager.m
@@ -15,6 +15,8 @@
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
   if ([[call method] isEqualToString:@"clearCookies"]) {
     [self clearCookies:result];
+  } else if ([[call method] isEqualToString:@"setCookies"]) {
+    [self setCookies:call];
   } else {
     result(FlutterMethodNotImplemented);
   }
@@ -44,6 +46,28 @@
       };
 
   [dataStore fetchDataRecordsOfTypes:websiteDataTypes completionHandler:deleteAndNotify];
+}
+
+- (void) setCookies:(FlutterMethodCall*)call {
+    NSString *url = call.arguments[@"url"];
+    NSDictionary *cookies = call.arguments[@"cookies"];
+    if (cookies != (id)[NSNull null]) {
+        [cookies enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL* stop) {
+            NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+            [cookieProperties setObject:key forKey:NSHTTPCookieName];
+            [cookieProperties setObject:value forKey:NSHTTPCookieValue];
+            [cookieProperties setObject:url forKey:NSHTTPCookieDomain];
+            [cookieProperties setObject:url forKey:NSHTTPCookieOriginURL];
+            [cookieProperties setObject:@"/" forKey:NSHTTPCookiePath];
+            [cookieProperties setObject:@"0" forKey:NSHTTPCookieVersion];
+            // set expiration to one month from now or any NSDate of your choosing
+            // this makes the cookie sessionless and it will persist across web sessions and app launches
+            /// if you want the cookie to be destroyed when your app exits, don't set this
+            [cookieProperties setObject:[[NSDate date] dateByAddingTimeInterval:2629743] forKey:NSHTTPCookieExpires];
+            NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+        }];
+    }
 }
 
 @end
